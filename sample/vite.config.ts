3ce2df34af61479969ca3b74aa68092c6f5e96ff
Asuma Yamada <yamadaasuma@gmail.com>
2025-03-29 03:25:16 +0900

    fix: add type identifier

--- sample/vite.config.ts
+++ sample/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, PluginOption } from "vite";
+import { defineConfig, type PluginOption } from "vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { qrcode } from "vite-plugin-qrcode";
